84ea6031be58a6728810ff2c090a970428024174
MinEquals0 <164611000+MinEquals0@users.noreply.github.com>
2024-03-23 17:20:34 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.51 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        String answer1 = String.valueOf(a) + String.valueOf(b);
+        String answer2 = String.valueOf(b) + String.valueOf(a);
+        
+        int result1 = Integer.parseInt(answer1);
+        int result2 = Integer.parseInt(answer2);
+        
+        if(result1 > result2){
+            return result1;
+        } else {
+            return result2;
+        }
+  
+    }
+}  
