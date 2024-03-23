8950a19c998cf2c0aa47d9cf596a725d79c29944
MinEquals0 <164611000+MinEquals0@users.noreply.github.com>
2024-03-23 17:10:53 +0900

    [level 0] Title: 문자열 출력하기, Time: 153.24 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int len = str.length();
+        if(1<=len && len<=100000){
+            System.out.println(str);
+        } 
+            
+    }
+}
