dff64f0e1a00293884c9e31d519d0702c259c795
MinEquals0 <164611000+MinEquals0@users.noreply.github.com>
2024-03-28 11:16:09 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int n) {
+       String answer = "";
+        // n-1 이 인덱스
+        answer = my_string.substring(0, n);
+        return answer;
+    }
+}
