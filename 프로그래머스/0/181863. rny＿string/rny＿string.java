f8bb9fcb132824775f1f8bf18d8ddfad9b82d08f
MinEquals0 <164611000+MinEquals0@users.noreply.github.com>
2024-04-05 13:51:22 +0900

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,6 +1,24 @@
 class Solution {
     public String solution(String rny_string) {
-        rny_string = rny_string.replace("m", "rn");
-        return rny_string;
+        int count = 0;
+        int idx = 0;
+
+        for(int i = 0; i < rny_string.length(); i++){
+            if(rny_string.charAt(i) == 'm'){
+                count++;
+            }
+        }
+
+        char[] ch = new char[rny_string.length()+count];
+
+        for(int i = 0; i < rny_string.length(); i++){ // 문자열에서 한글자씩 가져와서 m인지 확인하는 거니까 문자열 길이만큼
+            if(rny_string.charAt(i) == 'm'){
+                ch[idx++] = 'r'; // char는 글자 하나만 가능!
+                ch[idx++] = 'n';
+            } else {
+                ch[idx++] = rny_string.charAt(i);
+            }
+        }
+        return new String(ch);
     }
 }
