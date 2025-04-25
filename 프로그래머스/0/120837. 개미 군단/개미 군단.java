8f01850e53a632d65c371127bb31b9d30124f357
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-25 16:16:04 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,16 +1,19 @@
 class Solution {
     public int solution(int hp) {
-        int gen = hp / 5;
-        hp %= 5;
+//         int gen = hp / 5;
+//         hp %= 5;
         
-        int sol = hp / 3;
-        hp %= 3;
+//         int sol = hp / 3;
+//         hp %= 3;
         
-        int work = hp / 1;
+//         int work = hp / 1;
         
-        int army = gen + sol + work;
+//         int army = gen + sol + work;
         
-        return army;
+//         return army;
+        
+        return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
+       
         
     }
 }
