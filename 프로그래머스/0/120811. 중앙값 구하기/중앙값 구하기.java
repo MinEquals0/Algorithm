87a5b6eeb24df6d85b61c7d63892c0d176a623f8
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-25 15:34:49 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.51 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -2,16 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        ArrayList <Integer> list = new ArrayList<>();
-        
-        for(int i : array){
-            list.add(i);
-        }
-        
-        Collections.sort(list);
-        int mid = list.size()/2;
-        
-        return list.get(mid); 
-        
+        Arrays.sort(array);
+        int mid = array.length/2;
+        return array[mid]; 
     }
 }
