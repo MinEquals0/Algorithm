ece84f39647e241971deab47245f14a085baefc5
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-26 13:07:27 +0900

    Update 문자열 정렬하기 （1）.java

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -3,15 +3,31 @@ import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
 
+        // List<Integer> list = new ArrayList<>();
+        
+        // for(int i = 0; i < my_string.length(); i++){
+        //     if(Character.isDigit(my_string.charAt(i))){
+        //         int num = my_string.charAt(i) - '0';
+        //         list.add(num);
+        //     }
+        // }
+        // list.sort(null);
+        
+        // // list -> int[]
+        // int[] arr = new int[list.size()];
+        
+        // for(int i = 0; i < list.size(); i++){
+        //     arr[i] = list.get(i);    
+        // }
+
         List<Integer> list = new ArrayList<>();
         
         for(int i = 0; i < my_string.length(); i++){
-            if(Character.isDigit(my_string.charAt(i))){
-                int num = my_string.charAt(i) - '0';
-                list.add(num);
+            char c = my_string.charAt(i);
+            if(Character.isDigit(c)){
+                list.add(c - '0');
             }
         }
-        list.sort(null);
         
         // list -> int[]
         int[] arr = new int[list.size()];
@@ -19,6 +35,7 @@ class Solution {
         for(int i = 0; i < list.size(); i++){
             arr[i] = list.get(i);    
         }
+        Arrays.sort(arr);
         
         return arr;
     }
