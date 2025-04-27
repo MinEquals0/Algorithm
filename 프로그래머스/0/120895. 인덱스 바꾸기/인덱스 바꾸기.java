e53324eab18243e2b456c22648941cd9bf189997
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-27 12:26:41 +0900

    Update 인덱스 바꾸기.java

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,18 +1,27 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        char[] arr = new char[my_string.length()];
+        // char[] arr = new char[my_string.length()];
         
-        for(int i = 0; i < my_string.length(); i++){
-            if(i == num1 || i == num2 ){
-                arr[num2] = my_string.charAt(num1);
-                arr[num1] = my_string.charAt(num2);
+        // for(int i = 0; i < my_string.length(); i++){
+        //     if(i == num1 || i == num2 ){
+        //         arr[num2] = my_string.charAt(num1);
+        //         arr[num1] = my_string.charAt(num2);
                 
-            } else {
-                arr[i] = my_string.charAt(i);
+        //     } else {
+        //         arr[i] = my_string.charAt(i);
                 
-            }
-        }
-        String str = String.valueOf(arr);
+        //     }
+        // }
+        // String str = String.valueOf(arr);
+
+        String str = "";
+        
+        char[] arr = my_string.toCharArray();
+        char c = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = c;
+        
+        str = String.valueOf(arr);
         
         return str;
     }
