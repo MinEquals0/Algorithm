de5ccaf0f475451dbf996d1f9c81ed0c91eb2d07
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-28 14:55:30 +0900

    Update 숫자 찾기.java

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,17 +1,24 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
+        // int answer = 0;
         
-        char[] arr = Integer.toString(num).toCharArray();
+        // char[] arr = Integer.toString(num).toCharArray();
         
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] - '0' == k){
-                answer = i+1;
-                return answer;
-            }
+        // for(int i = 0; i < arr.length; i++){
+        //     if(arr[i] - '0' == k){
+        //         answer = i+1;
+        //         return answer;
+        //     }
             
-        }
+        // }
         
-        return  -1;
+        // return  -1;
+        
+        String str = String.valueOf(num);
+        String kStr = String.valueOf(k);
+        
+        int answer = str.indexOf(kStr);
+        
+        return answer < 0 ? -1 : answer+1;
     }
 }
