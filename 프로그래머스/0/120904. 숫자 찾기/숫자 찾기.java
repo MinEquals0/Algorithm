7e8cf44f60e84857b6ebe5a3fd9fdf673d0eb9ed
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-28 14:49:09 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        
+        char[] arr = Integer.toString(num).toCharArray();
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] - '0' == k){
+                answer = i+1;
+                return answer;
+            }
+            
+        }
+        
+        return  -1;
+    }
+}
