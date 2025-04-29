c0f702ad6b73aedf6a39c0b1fc96fecff9331d76
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-29 13:58:12 +0900

    Update 배열 회전시키기.java

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -4,9 +4,9 @@ class Solution {
         // 만약 인덱스가 0보다 작아지면, 인덱스 length-1, 나머지는 전체 -1 
         // right - 끝의 자리 수가 첫자리로 이동, 전체 인덱스 +1
         // right일 때, 만약에 인덱스가 배열의 길이-1보다 길어지면, 인덱스 0, 나머지는 +1
-        
-        int[] answer = new int[numbers.length];
+
         int len = numbers.length;
+        int[] answer = new int[len];
         
         if(direction.equals("left")) {
             answer[0] = numbers[1];
