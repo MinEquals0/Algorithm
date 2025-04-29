90a9500849a2df942919479c0b26e7ecedcff8bd
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-29 13:05:43 +0900

    Update 피자 나눠 먹기 （2）.java

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,14 +1,31 @@
 class Solution {
+    // public int solution(int n) {
+    //     // (n * x) / 6 의 몫을 구하기
+        
+    //     int i = 1;
+        
+    //     while(true){
+    //         if((n * i) % 6 == 0) {
+    //             return (n * i) / 6;
+    //         }   
+    //         i++;
+    //     }
+    //}
+
     public int solution(int n) {
-        // (n * x) / 6 의 몫을 구하기
+        int answer = lcm(n, 6);
+        return answer/6;
+    }
+    
+    private int lcm(int a, int b){
+        return (a * b) / gcd(a, b);
+    }
         
-        int i = 1;
-        
-        while(true){
-            if((n * i) % 6 == 0) {
-                return (n * i) / 6;
-            }   
-            i++;
+    private int gcd(int a, int b){
+        if(b == 0) {
+            return a;
+        } else {
+            return gcd(b, a%b);
         }
     }
 }
