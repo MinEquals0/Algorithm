a8f0b27100889b447fcd7ff2c3f0d04750e6d60e
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-30 11:59:14 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        char[] arr = String.valueOf(n).toCharArray();
+        
+        int answer = 0;
+        for(int i = 0; i < arr.length; i++){
+            answer += arr[i] - '0';
+        }
+        
+        return answer;
+    }
+}
