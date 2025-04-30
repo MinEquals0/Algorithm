d777f4535de9b24f66078c3d0bcb1508b5b91968
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-04-30 12:00:00 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,19 @@
 class Solution {
     public int solution(int n) {
-        char[] arr = String.valueOf(n).toCharArray();
+        // char[] arr = String.valueOf(n).toCharArray();
         
+        // int answer = 0;
+        // for(int i = 0; i < arr.length; i++){
+        //     answer += arr[i] - '0';
+        // }
+        
+        // return answer;
+
         int answer = 0;
-        for(int i = 0; i < arr.length; i++){
-            answer += arr[i] - '0';
+        
+        while(n > 0){
+            answer += n % 10;
+            n /= 10;
         }
         
         return answer;
