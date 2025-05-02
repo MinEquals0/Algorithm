ae2afe9aeac1f685516a5ce7d0bba03cf0b850d6
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-02 13:01:53 +0900

    Update A로 B 만들기.java

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -2,6 +2,20 @@ import java.util. *;
 
 class Solution {
     public int solution(String before, String after) {
+        // int cnt = 0;
+        // StringBuilder sb = new StringBuilder(after);
+        
+        // for(int i = 0; i < before.length(); i++){
+        //     char ber = before.charAt(i);
+        //     for(int j = 0; j < sb.length(); j++){
+        //         if(ber == sb.charAt(j)){
+        //             cnt++;
+        //             sb.deleteCharAt(j);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return cnt == before.length() ? 1 : 0;
         
         char[] arr1 = before.toCharArray();
         char[] arr2 = after.toCharArray();
