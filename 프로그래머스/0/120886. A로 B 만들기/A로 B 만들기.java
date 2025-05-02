1e16566e7955f5efecd3f32e92a1d5a9f15abeab
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-02 13:00:34 +0900

    [level 0] Title: A로 B 만들기, Time: 1.20 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,22 +1,13 @@
+import java.util. *;
+
 class Solution {
     public int solution(String before, String after) {
-        int cnt = 0;
-        StringBuilder sb = new StringBuilder(after);
         
+        char[] arr1 = before.toCharArray();
+        char[] arr2 = after.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         
-        for(int i = 0; i < before.length(); i++){
-            char ber = before.charAt(i);
-            
-           
-            for(int j = 0; j < sb.length(); j++){
-                if(ber == sb.charAt(j)){
-                    cnt++;
-                    sb.deleteCharAt(j);
-                    break;
-                }
-            }
-        }
-        
-        return cnt == before.length() ? 1 : 0;
+        return Arrays.equals(arr1, arr2) ? 1 : 0;
     }
 }
