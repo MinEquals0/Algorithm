58d0ca5f5defa1768ac9bbde2567ecd4f572a4cf
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-04 14:13:58 +0900

    [level 0] Title: k의 개수, Time: 5004.18 ms, Memory: 399 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,17 +1,10 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        // 숫자들을 문자로 변환한다음, for문 돌면서 비교?
-        String s = k + "";
-        int answer = 0;
-        
+        String str = "";
         for(int a = i; a <= j; a++){
-            String str = a + "";
-            for(char c : str.toCharArray()){
-                if((c + "").equals(s)){
-                    answer++;
-                }
-            }
+            str += a + "";
         }
-        return answer;
+        
+        return str.length() - (str.replace(k+"", "")).length();
     }
 }
