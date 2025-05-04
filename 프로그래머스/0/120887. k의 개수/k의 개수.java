02aad23e0e67577c6e6eaedf8da6b276f680ac05
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-04 14:15:00 +0900

    Update k의 개수.java

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,5 +1,19 @@
 class Solution {
     public int solution(int i, int j, int k) {
+        // 숫자들을 문자로 변환한다음, for문 돌면서 비교
+        // String s = k + "";
+        // int answer = 0;
+        
+        // for(int a = i; a <= j; a++){
+        //     String str = a + "";
+        //     for(char c : str.toCharArray()){
+        //         if((c + "").equals(s)){
+        //             answer++;
+        //         }
+        //     }
+        // }
+        // return answer;
+        
         String str = "";
         for(int a = i; a <= j; a++){
             str += a + "";
