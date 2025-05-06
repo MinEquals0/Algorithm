d8363323ee600eb9fc95ce5a49b647e716922149
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-06 16:11:14 +0900

    Update 가까운 수.java

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -9,7 +9,7 @@ class Solution {
             if(diff < minDiff){
                 minDiff = diff;
                 close = array[i];
-            } else if(diff == minDiff && array[i] < close){
+            } else if(diff == minDiff && array[i] < close){ // 가까운 수가 여러개 일때는 가장 작은 수
                 close = array[i];
             }
         }
