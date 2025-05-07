972426933014101bb06ec8b4d822f0619caed1f4
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-07 12:13:55 +0900

    Update 한 번만 등장한 문자.java

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,23 +2,35 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String copy = s;
+        // String copy = s;
         
-        for(int i = 0; i < copy.length(); i++){
-            char target = copy.charAt(i);
+        // for(int i = 0; i < copy.length(); i++){
+        //     char target = copy.charAt(i);
             
-            for(int j = i+1; j < copy.length(); j++){
-                if(target == copy.charAt(j)){
-                    s = s.replaceAll(target+"", "");
-                }
-            }
-        }
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
+        //     for(int j = i+1; j < copy.length(); j++){
+        //         if(target == copy.charAt(j)){
+        //             s = s.replaceAll(target+"", "");
+        //         }
+        //     }
+        // }
+        // char[] arr = s.toCharArray();
+        // Arrays.sort(arr);
        
+        // return new String(arr);
+
+        int[] alpha = new int[26];
+        for(char c : s.toCharArray()){
+            alpha[c - 'a']++;
+        }
         
-        return new String(arr);
+        StringBuilder sb = new StringBuilder();
         
+        for (int i = 0; i < 26; i++) {
+            if (alpha[i] == 1) {
+                sb.append((char) (i + 'a'));    
+            } 
+        }
         
+        return sb.toString();
     }
 }
