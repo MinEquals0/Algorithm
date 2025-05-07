f9d041a96c1e1fc8128611cd97f3798e4a4c6c1d
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-07 11:55:39 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 9.34 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String copy = s;
+        
+        for(int i = 0; i < copy.length(); i++){
+            char target = copy.charAt(i);
+            
+            for(int j = i+1; j < copy.length(); j++){
+                if(target == copy.charAt(j)){
+                    s = s.replaceAll(target+"", "");
+                }
+            }
+        }
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        String result = new String(arr);
+        
+        return result;
+        
+        
+    }
+}
