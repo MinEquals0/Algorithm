c2d5a7720f5e8950e027090fd9a3b6e2dc47819e
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-09 14:10:31 +0900

    Update 소인수분해.java

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -3,24 +3,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        
+
+        // 중복 제거, 오름차순도 동시에 해결
         LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        
         int i = 2;
-        
-        while(i * i <= n){
-            while(n % i == 0){
+
+        while(n != 0 && i <= n){
+            if(n % i == 0){
                 set.add(i);
-                n = n / i;
+                n = n / i; // n을 i로 계속 나누기
+            } else {
+                i++;  // i를 증가시켜서 더 큰 수를 시도
             }
-            i++;
-        }
-        
-        if(n > 1){
-            set.add(n);
         }
         
         int[] answer = set.stream().mapToInt(Integer::intValue).toArray();
-        
         
         return answer;
     }
