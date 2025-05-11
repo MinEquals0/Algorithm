0646532640633b48bab03eedc37d167584a1cbe1
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-11 11:57:21 +0900

    [level 0] Title: 이진수 더하기, Time: 2.33 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String bin1, String bin2) {
+        // 길이 맞추기
+        while(bin1.length() < bin2.length()){
+            bin1 = "0" + bin1;
+        }
+        
+        while(bin1.length() > bin2.length()){
+            bin2 = "0" + bin2;
+        }
+        
+        
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = bin1.length()-1; i >= 0; i--){
+            int b1 = bin1.charAt(i) - '0';
+            int b2 = bin2.charAt(i) - '0';
+            
+            int sum = b1 + b2 + carry;
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        if(carry == 1){
+            sb.append(carry);
+        }
+        
+        sb.reverse();
+        
+        return sb.toString();
+    }
+}
