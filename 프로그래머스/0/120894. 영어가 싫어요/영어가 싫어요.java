d9dd17536aa49a927c5d118a02e4155d5affbf04
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-16 12:18:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 13.16 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String numbers) {
+        
+        
+        StringBuilder sb = new StringBuilder();
+        String[] idx = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String answer = "";
+        
+        for(char c : numbers.toCharArray()){
+            sb.append(c);
+            for(int i = 0; i < idx.length; i++){
+                if(sb.toString().equals(idx[i])){
+                    answer += i;
+                    sb.setLength(0);
+                }
+            }
+        }
+        
+       long result = Long.parseLong(answer);
+       
+        return result;
+    }
+}
