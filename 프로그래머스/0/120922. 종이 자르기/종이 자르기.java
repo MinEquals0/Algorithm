35d99b893f1cdefaf70f8e8fa80d80509117662f
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-05-22 13:34:55 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int M, int N) {
+        // M, N이 1이 되도록 자르려면,
+        // (M-1) + {(N-1) * M}
+        int row = M - 1;
+        int col = (N-1) * M;
+        
+        int answer = row + col;
+        return answer;
+    }
+}
