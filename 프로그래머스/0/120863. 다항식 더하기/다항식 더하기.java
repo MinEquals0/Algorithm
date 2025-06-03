c97f12b42516c7bdbd1266c29eea2e2d4b4f26be
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-06-03 12:59:15 +0900

    Update 다항식 더하기.java

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(String polynomial) {
+        // 문자열을 split으로 + 기준으로 나누기
+        // 문자열을 순회하면서 x가 있는 요소와 없는 요소끼리 더해줌
         String[] arr = polynomial.split("\\+");
         
-        int xSum = 0;
-        int numSum = 0;
+        int xSum = 0;  // x항들의 누적합
+        int numSum = 0;  // 일반 항들의 누적합
         
         for(String str : arr){
-            str = str.trim();
+            str = str.trim(); // 공백 제거
             if(str.contains("x")){
-                String s = str.replace("x","").trim();
+                String s = str.replace("x","").trim(); // 공백 제거
                 if(s.equals("")){ // 1x 값 처리
                     s = "1";
                 }
-                xSum += Integer.parseInt(s.trim());
+                xSum += Integer.parseInt(s.trim()); // 누적합 구하기
             } else {
                 numSum += Integer.parseInt(str.trim());
             }
            
         }
         
-        if(xSum == 0){
+        if(xSum == 0){ // x항이 없을 때
             return String.valueOf(numSum);
-        } else if (numSum == 0){
+        } else if (numSum == 0){ // 숫자항이 없을 때
             return xSum == 1 ? "x" : xSum + "x";
         } else {
             return (xSum == 1 ? "x" : xSum + "x") + " + " + numSum;
