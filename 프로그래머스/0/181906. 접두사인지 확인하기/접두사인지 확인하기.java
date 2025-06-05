23722e7617d0f68f62ba76abea90feb61f7495d7
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-06-05 20:58:35 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,24 +1,13 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        // 제한 조건
-        if(is_prefix.length() > my_string.length()){
-            return 0;
-        }
-
-        // my_string으로 만들 수 있는 모든 접두사 -> 새 배열에 담기
-        String[] prefixArr = new String[my_string.length()];
-
-        for(int i = 1; i < my_string.length(); i++){
-            String str = my_string.substring(0,i);
-            prefixArr[i-1] = str;
-        }
-
-        // 새 배열을 돌면서 is_prefix가 있는지 확인
-        for(String s : prefixArr){
-            if(is_prefix.equals(s)) {
+        
+        for(int i = 0; i < my_string.length(); i++){
+            String s = my_string.substring(0, i);
+            if(s.equals(is_prefix)){
                 return 1;
             }
         }
+        
         return 0;
     }
 }
