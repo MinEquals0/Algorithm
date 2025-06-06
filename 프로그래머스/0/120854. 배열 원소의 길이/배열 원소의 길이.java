c916dba2ef5086bf33dbaf5ddc1510ac3b126bda
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-06-06 10:47:39 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,12 +1,12 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] strlist) {
-        // for문을 순회하면서 각 요소의 길이를 저장
-        // 결과값을 담을 int 배열 생성 - 길이 : strlist.length
-
         int[] answer = new int[strlist.length];
-
+        
         for(int i = 0; i < strlist.length; i++){
             answer[i] = strlist[i].length();
+                 
         }
         return answer;
     }
