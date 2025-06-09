b2d0eb4c1b4af2719c3c0bf7a3568d4a9a916437
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-06-09 14:48:07 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.81 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,5 +1,19 @@
+
+import java.util.*;
+
 class Solution {
     public String[] solution(String my_string) {
-        return my_string.trim().split(" +");
+        String[] arr = my_string.split(" ");
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
+        
+        for(int i = list.size() -1 ; i >= 0; i--){
+            if(list.get(i).equals("")){
+                list.remove(i);
+            }
+        }
+        
+        String[] answer = list.toArray(new String[0]);
+        
+        return answer;
     }
 }
