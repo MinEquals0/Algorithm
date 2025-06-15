a11484ea7499b7f25d6ead3cf674709336860fca
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-06-15 11:18:18 +0900

    [level 0] Title: OX퀴즈, Time: 0.74 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] quiz) {
+        // quiz 순회 하면서 요소 하나씩 공백으로 split
+        // 답 이 맞는지 확인
+
+        
+        // 공백으로 나누고, 공백 제거
+        // [[3,-,4,=,-3], [5,+,6,=,11]]
+        String[] answer = new String[quiz.length];
+        
+        String[][] expArr = new String[quiz.length][];
+        
+        for(int i = 0; i < quiz.length; i++){
+            expArr[i] = quiz[i].trim().split("\\s+");
+        }
+        
+        // for문 순회하면서 답 검사
+        for(int i = 0; i < expArr.length; i++){ 
+            int result = 0;
+            int num1 = Integer.parseInt(expArr[i][0]); 
+            int num2 = Integer.parseInt(expArr[i][2]); 
+            int num3 = Integer.parseInt(expArr[i][4]);
+            
+            
+            // 기호에 따라 분류 +, -
+            if(expArr[i][1].equals("+")){
+                result = num1 + num2;
+            } else {
+                result = num1 - num2;
+            }
+            
+            if(result == num3){
+                answer[i] = "O";
+            } else {
+                answer[i] = "X";
+            }
+        }
+        
+        return answer;
+   
+    }
+}
