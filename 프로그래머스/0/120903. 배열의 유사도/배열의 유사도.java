33872039577eb269209288014f7415001b540495
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-06-18 13:50:12 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,18 +1,14 @@
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        // 이중 for문 - s1의 한 요소에 대해 s2 모든 요소 비교
-        // 같은 문자열이 있으면 count++ - count변수 생성
-        // count 리턴
-
-        int count = 0;
-
-        for(int i = 0; i < s1.length; i++){
-            for(int j = 0; j < s2.length; j++){
-                if(s1[i].equals(s2[j])){  // 문자열 == : 주소 비교
-                    count++;
+        int answer = 0;
+        
+        for(String s : s1){
+            for(String str : s2) {
+                if(s.equals(str)){
+                    answer++;
                 }
             }
         }
-        return count;
+        return answer;
     }
 }
