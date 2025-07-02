5056133c999dbb85447eea67f2c09cd4b1f6a337
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-02 15:05:31 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        double result = Math.sqrt(n);
+        
+        if(result % 1 == 0.0){
+            return 1;
+        }
+        return 2;
+    }
+}
