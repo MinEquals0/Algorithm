dba7aecae7eae575a47288b41ec46c3474cda6b5
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-08 17:38:41 +0900

    [level 0] Title: n 번째 원소부터, Time: 2.91 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        //copyofRange를 이용 -> num_list 에 n-1 인덱스부터 num_list.length까지 복사 리턴
-        return Arrays.copyOfRange(num_list,n-1,num_list.length);
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = n-1; i < num_list.length; i++){
+            list.add(num_list[i]);
+        }
+        
+        return list.stream().mapToInt(Integer:: intValue).toArray();
     }
 }
