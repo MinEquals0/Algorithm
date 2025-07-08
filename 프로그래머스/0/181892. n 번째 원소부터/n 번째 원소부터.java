eb90cb54f82f881b8f92681aa08cd3e1b22b591c
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-08 17:41:12 +0900

    Update n 번째 원소부터.java

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list = new ArrayList<>();
         
-        for(int i = n-1; i < num_list.length; i++){
-            list.add(num_list[i]);
-        }
+        // for(int i = n-1; i < num_list.length; i++){
+        //     list.add(num_list[i]);
+        // }
         
-        return list.stream().mapToInt(Integer:: intValue).toArray();
+        // return list.stream().mapToInt(Integer:: intValue).toArray();
+
+        int[] arr = Arrays.copyOfRange(num_list, n-1, num_list.length);
+        return arr;
     }
 }
