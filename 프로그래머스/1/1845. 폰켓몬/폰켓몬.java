4542b07e23e60f40f40d1a77c9b65288a2f7b9cf
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-12 12:50:40 +0900

    [level 1] Title: 폰켓몬, Time: 4.61 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,31 +3,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 1. HashMap 사용
-        // nums에서 다른 수가 몇 개인지 카운트 = 0이 아닌 수를 카운트
         
-        // int answer = 0;
-        
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // // 몇 번 나오는지 카운트
-        // for(int i : nums){
-        //     map.put(i, map.getOrDefault(i,0)+1);
-        //     System.out.println(map.get(i));
-        // }
-        
-        // // 0이 아닌 수 카운트
-        // for(int key : map.keySet()){
-        //     if(map.get(key) != 0){
-        //         answer++;
-        //     }
-            
-        // }
-        
-        // return Math.min(answer, nums.length/2);
-
-
-        // 2. HashSet 사용
         Set<Integer> set = new HashSet<>();
         
         for(int i : nums){
@@ -35,5 +11,6 @@ class Solution {
         }
         
         return Math.min(set.size(), nums.length/2);
+        
     }
 }
