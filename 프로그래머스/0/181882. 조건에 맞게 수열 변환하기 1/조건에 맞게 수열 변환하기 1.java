e37f7726788e3b6d9bd671f6c7c874bb3f009f51
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-14 21:10:57 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 18.52 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] solution(int[] arr) {
-        // arr 을 for문으로 돌면서 if 문으로 판별
-        // 50보다 크거나 같은 짝수 -> arr[i] / 2
-        // 50보다 작은 홀수 -> arr[i] * 2
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] >= 50 && arr[i] % 2 == 0){
-                arr[i] /= 2;  // arr[i] = arr[i] / 2
-            } else if(arr[i] < 50 && arr[i] % 2 == 1){
-                arr[i] *= 2;
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        
+        for(int i : arr){
+           if(i >= 50 && i % 2 == 0){
+               answer[idx] = i / 2;
+           } else if(i < 50 && i % 2 == 1){
+               answer[idx] = i * 2;
+           }  else {
+                answer[idx] = i; 
             }
+            idx++;
         }
-        return arr;
+        return answer;
     }
 }
