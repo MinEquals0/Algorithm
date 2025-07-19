4de124dad3448a6d1c5f703220be5b3f29fe7be9
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-19 21:42:27 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.25 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,19 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] newArr = new int[num_list.length];
-        // for문을 돌면서 앞 - n ~ 끝까지 자르고(인덱스)
-        // 뒤 - 0~n-1까지
+        int[] answer = new int[num_list.length];
         int idx = 0;
-
+        
         for(int i = n; i < num_list.length; i++){
-            newArr[idx] = num_list[i];
+            answer[idx] = num_list[i];    
+            idx++;
+            
+        }
+        
+        for(int i = 0; i < n; i++){
+            answer[idx] = num_list[i];
             idx++;
         }
-
-        for(int i = 0; i <= n-1; i++){
-            newArr[idx] = num_list[i];
-            idx++;
-        }
-        return newArr;
+        
+        System.out.println(Arrays.toString(answer));
+        
+        return answer;
     }
 }
