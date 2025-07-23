f71e9a105b5d1071ac1b7c7918e65266f15bce4c
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-23 13:10:58 +0900

    [level 0] Title: 수 조작하기 2, Time: 1192.32 ms, Memory: 397 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,26 +1,19 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        for (int i = 0 ; i < numLog.length-1; i++){
-            switch(numLog[i+1] - numLog[i]) {
-                case 1 :
-                    answer += "w";
-                    break;
-
-                case -1:
-                    answer += "s";
-                    break;
-
-                case 10:
-                    answer += "d";
-                    break;
-
-                case -10:
-                    answer += "a";
-                    break;
+        
+        for(int i = 1; i < numLog.length; i++){
+            if(numLog[i-1] + 1 == numLog[i]){
+                answer += "w";
+            } else if(numLog[i-1] - 1 == numLog[i]){
+                answer += "s";
+            } else if(numLog[i-1] + 10 == numLog[i]){
+                answer += "d";
+            } else if(numLog[i-1] - 10 == numLog[i]){
+                answer += "a";
             }
         }
-
+        
         return answer;
     }
 }
