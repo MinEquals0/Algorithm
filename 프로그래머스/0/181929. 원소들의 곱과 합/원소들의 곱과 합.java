df66b475050dbf865bc02e4ef0f7ea8e7962dc70
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-24 12:47:24 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int[] num_list) {
+        int product = 1;
         int sum = 0;
-        int multi = 1; 
+        
         for(int i : num_list){
+            product *= i;
             sum += i;
-            multi *= i;
         }
         
-        int sq = sum * sum;
-        
-        return multi < sq ? 1 : 0;
+        return product < Math.pow(sum, 2) ? 1 : 0;
     }
 }
