e9ff3e89ff90469202cfd3e86395d18191986452
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-25 13:10:53 +0900

    [level 0] Title: 접미사 배열, Time: 0.57 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -2,19 +2,15 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        ArrayList<String> al = new ArrayList<>();
-
-        // my_string 문자열 앞에서 인덱스 하나씩 제거 - arraylist에 저장
-        int len = my_string.length();
-        for(int i = 0; i < len; i++){
-            String str = my_string.substring(i,len);
-            al.add(str);
+        String[] answer = new String[my_string.length()];
+        
+        for(int i = 0; i < my_string.length(); i++){
+            String s = my_string.substring(i);
+            answer[i] = s;
         }
-        // 정렬
-        Collections.sort(al);
-
-        // arraylist -> array
-        String[] result = al.stream().toArray(String[]::new);
-        return result;
+        
+        Arrays.sort(answer);
+        
+        return answer;
     }
 }
