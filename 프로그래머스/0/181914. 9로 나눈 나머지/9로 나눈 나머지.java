4e215be28982a636288a1aa450524c55d69eb2e4
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-07-27 12:10:31 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 6.41 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,14 +1,16 @@
 class Solution {
     public int solution(String number) {
-        int answer = 0;
+        
         int sum = 0;
+        
         for(int i = 0; i < number.length(); i++){
-            char ch = number.charAt(i);
-            int n = Character.getNumericValue(ch);
-            sum += n;
-            answer = sum % 9;
+            int num = Character.getNumericValue(number.charAt(i));
+            sum += num;            
         }
         
+        int answer = sum % 9;
+        
+
         return answer;
     }
 }
