940914712ec83b1cf1c2894a098a7d95bbac319f
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-08-04 19:54:47 +0900

    [level 0] Title: 정수 부분, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/181850. 정수 부분/정수 부분.java
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(double flo) {
-        // Math.floor()를 사용
-        return (int) Math.floor(flo);
+        int answer = (int) flo;
+        return answer;
     }
 }
