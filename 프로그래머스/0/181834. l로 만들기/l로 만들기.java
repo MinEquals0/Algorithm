b9e8a96b1b6d549b8e414911381cbaca1766034a
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-08-06 20:41:06 +0900

    [level 0] Title: l로 만들기, Time: 1099.01 ms, Memory: 381 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,17 +1,15 @@
 class Solution {
     public String solution(String myString) {
-        // I 보다 앞서는 알파벳을 I로 치환
-        // myString 문자열을 char[] 변환
-        // char[]을 for문으로 돌면서 한글자씩 I보다 앞선 글자인지 확인 - if
-        // < 'l'
-
-        char[] ch = myString.toCharArray();
-
-        for(int i = 0; i < ch.length; i++){
-            if(ch[i] < 'l'){
-                ch[i] = 'l';
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++){
+            char c = myString.charAt(i);
+            if(c < 'l'){
+                answer += "l";
+            } else {
+                answer += c;
             }
         }
-        return new String(ch);
+        return answer;
     }
 }
