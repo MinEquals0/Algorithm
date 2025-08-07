d9bc7bc0520474d35931db6f260e266451d4da38
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-08-07 21:28:01 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        // for each 문으로 num_list를 돌면서 n이 있는지 확인
-        for (int i : num_list) {
+        int answer = 0;
+        
+        for(int i : num_list){
             if(i == n){
-                return 1;
+                answer = 1;
             } 
         }
-        return 0;
+        return answer;
     }
 }
