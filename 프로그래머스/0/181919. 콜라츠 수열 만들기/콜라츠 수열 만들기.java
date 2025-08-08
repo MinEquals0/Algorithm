711d43f86e724b549339f22284f9db67c027c59b
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-08-08 10:32:05 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 4.44 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -2,14 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        answer.add(n);
-        while (n != 1) {
-            n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
-            answer.add(n);
+        List<Integer> list = new ArrayList<>();
+        list.add(n);
+        
+        while(n != 1){
+            if(n % 2 == 0){
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            
+            list.add(n);
+            System.out.println(n);
         }
-
-        int[] newArr = answer.stream().mapToInt(Integer::intValue).toArray();
-        return newArr;
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
