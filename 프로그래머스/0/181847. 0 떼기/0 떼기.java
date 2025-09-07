866da9f924203f036652c86c3ff47fee9785814a
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-09-07 21:44:07 +0900

    [level 0] Title: 0 떼기, Time: 0.10 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,23 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        // for문 사용, 조건 0이 아닌 숫자가 나올 때까지 돌아서 0이 아닌 수가 나오는 인덱스 알아내기
-        // for문 범위 알아낸 인덱스 ~ n_str 끝까지
-        // String -> int 변환
-        // 문자열을 저장할 StringBuilder 생성
-        StringBuilder sb = new StringBuilder();
-        int idx = 0;
-
-        for(int i = 0; i < n_str.length(); i++){
-            if(n_str.charAt(i) > '0') { 
-                idx = i;
-                break;
-            }
-        }
-
-        for(int i = idx; i < n_str.length(); i++){
-            sb.append(n_str.charAt(i));
-        }
-
-        return sb.toString();
+        return n_str.replaceFirst("^0+", "");
     }
 }
