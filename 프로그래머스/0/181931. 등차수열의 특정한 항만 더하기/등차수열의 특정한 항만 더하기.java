589be510953ece482bba8db556cba0f88260217d
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-10-20 16:41:41 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -1,18 +1,15 @@
 class Solution {
     public int solution(int a, int d, boolean[] included) {
-        int total = 0;
+        int answer = 0;
         
-        // 불리언 배열 돌면서 i 값 구하기
+        // an = a + (n-1)d
+        
         for(int i = 0; i < included.length; i++){
-            if(included[i] == true) {
-                int seq = a + ((i+1)- 1) * d; 
-                total += seq;
-            } 
-        } 
+            if(included[i]){
+                answer += a + i*d;
+            }
+        }
         
-        return total;
+        return answer;
     }
 }
-// a1, d로 등차수열 만들기 -> 불리언 배열 길이
-// 불리언 배열에서 true 값이 있는 인덱스 -> 등차수열에서 i+1 항 값 더하기
-// 0 3 4 -> 1 4 5 => 7 15 19 = 17
