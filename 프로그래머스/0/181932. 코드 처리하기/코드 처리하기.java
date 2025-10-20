cccc24618bd9ae5df555fb669b37a0384905b863
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-10-20 16:31:05 +0900

    [level 0] Title: 코드 처리하기, Time: 10.30 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,30 +1,38 @@
 class Solution {
     public String solution(String code) {
-        StringBuilder answer = new StringBuilder();
-        boolean mode = false;
+        // String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int mode = 0;
+        
+        for(int i = 0; i < code.length(); i++){
+            if(mode == 0){
+                if(code.charAt(i) == '1'){
+                    mode = 1;
+                } else {
+                    if(i % 2 == 0){
+                        sb.append(code.charAt(i));
+                        
+                    }
+                }
+            
+            } else {   // mode = 1
+                if(code.charAt(i) == '1'){
+                    mode = 0;
+                } else {
+                    if(i % 2 == 1){
+                        sb.append(code.charAt(i));
 
-        for (int i = 0; i < code.length(); i++) {
-            char ch = code.charAt(i);
-
-            if (i == 0 && ch == '1') {
-                mode = !mode;
-                continue;
-            }
-
-            if (ch == '1') {
-                mode = !mode;
-                continue;
-            }
-
-            if ((mode == false && i % 2 == 0) || (mode == true && i % 2 == 1)) {
-                answer.append(ch);
+                    } 
+                }
             }
         }
-
-        if (answer.length() == 0) {
+        
+        if(sb.toString().equals("")){
             return "EMPTY";
         }
-
-        return answer.toString();
+        
+        return sb.toString();
     }
 }
