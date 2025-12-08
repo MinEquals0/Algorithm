a13038d2e13c504d6404018d2b13d3df40437726
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-08 19:47:52 +0900

    [level 0] Title: 수 조작하기 1, Time: 10.50 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/README.md
+++ 프로그래머스/0/181926. 수 조작하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 1 - 181926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=java) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 2.27 ms
+메모리: 89.5 MB, 시간: 10.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 23일 17:25:57
+2025년 12월 08일 19:47:49
 
 ### 문제 설명
 
--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,27 +1,17 @@
 class Solution {
     public int solution(int n, String control) {
-        int answer = n;
-        for (int i = 0; i < control.length(); i++){
-            Character ch = control.charAt(i);
-            switch(ch) {
-                case 'w' :
-                    answer += 1;
-                    break;
-
-                case 's':
-                    answer -= 1;
-                    break;
-
-                case 'd':
-                    answer += 10;
-                    break;
-
-                case 'a':
-                    answer -= 10;
-                    break;
+        
+        for(int i = 0; i < control.length(); i++){
+            if(control.charAt(i) == 'w'){
+                n += 1;
+            } else if(control.charAt(i) == 's'){
+                n -= 1;
+            } else if(control.charAt(i) == 'd'){
+                n += 10;
+            } else {
+                n -= 10;
             }
         }
-
-        return answer;
+        return n;
     }
 }
