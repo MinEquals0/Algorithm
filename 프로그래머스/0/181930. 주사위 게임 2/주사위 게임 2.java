8228abc33d927762bf3d8af217b37f4d58381dfc
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-09 21:41:49 +0900

    Update 주사위 게임 2.java

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -2,12 +2,22 @@ class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;
         if (a == b && b == c){
-            answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+            //answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+             answer = (a+b+c) * (square(a) + square(b) + square(c)) * (cube(a) + cube(b) + cube(c));
         } else if (a != b && b != c && a != c){
             answer = a + b + c;
         } else {
-            answer = (a+b+c) * (a*a + b*b + c*c);
+            //answer = (a+b+c) * (a*a + b*b + c*c);
+             answer = (a+b+c) * (square(a) + square(b) + square(c));
         }
         return answer;
     }
+
+    public int square(int x){
+            return x * x;
+    }
+
+    public int cube(int x){
+        return x * x * x;
+    }  
 }
