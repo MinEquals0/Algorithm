ed73c0aa32c4f23a75f84e0abe2ca08833feb6c2
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-09 21:28:39 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;
-        int sum = a + b + c;
-        int doub = (int)(Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2));
-        int tri =  (int)(Math.pow(a,3)+ Math.pow(b,3) + Math.pow(c,3));
-        
-        if(a == b && b == c) {
-            answer = sum * doub * tri;
-        } else if(a == b || b == c || c == a){
-            answer = sum * doub;
+        if (a == b && b == c){
+            answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        } else if (a != b && b != c && a != c){
+            answer = a + b + c;
         } else {
-            answer = sum;
+            answer = (a+b+c) * (a*a + b*b + c*c);
         }
         return answer;
     }
