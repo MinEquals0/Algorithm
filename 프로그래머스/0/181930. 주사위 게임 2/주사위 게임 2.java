707690fc487b82d113e38fa1b54cd7afd6311862
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-09 21:48:02 +0900

    Update 주사위 게임 2.java

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int a, int b, int c) {
+        int sum = a + b + c;
+        int sumSquare = square(a) + square(b) + square(c);
+        int sumCube = cube(a) + cube(b) + cube(c);
         int answer = 0;
+        
         if (a == b && b == c){
             //answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
-             answer = (a+b+c) * (square(a) + square(b) + square(c)) * (cube(a) + cube(b) + cube(c));
+            //answer = (a+b+c) * (square(a) + square(b) + square(c)) * (cube(a) + cube(b) + cube(c));
+            answer = sum * sumSquare * sumCube;
         } else if (a != b && b != c && a != c){
-            answer = a + b + c;
+            //answer = a + b + c;
+            answer = sum;
         } else {
             //answer = (a+b+c) * (a*a + b*b + c*c);
-             answer = (a+b+c) * (square(a) + square(b) + square(c));
+            //answer = (a+b+c) * (square(a) + square(b) + square(c));
+            answer = sum * sumSquare;
         }
         return answer;
     }
