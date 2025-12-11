7af63e673471eb69b765fa4fd494d4ab7fb5b3e4
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-11 22:17:07 +0900

    Update 배열 만들기 2.java

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -4,24 +4,29 @@ class Solution {
     public int[] solution(int l, int r) {
         List<Integer> list = new ArrayList<>();
         
+        // for(int i = l; i <= r; i++){
+        //     String s = String.valueOf(i);
+        //     StringBuilder sb = new StringBuilder(s);
+
+        //     for (int j = 0; j < sb.length(); j++) {
+        //         char c = sb.charAt(j);
+        //         if (c == '0' || c == '5') {
+        //             sb.deleteCharAt(j);  
+        //             j--;
+        //         }
+        //     }
+
+        //     if(sb.length() == 0){
+        //         list.add(i);
+        //     }
+        // }
+
+        // 2. 정규식
         for(int i = l; i <= r; i++){
-            String s = String.valueOf(i);
-            StringBuilder sb = new StringBuilder(s);
-
-            for (int j = 0; j < sb.length(); j++) {
-                char c = sb.charAt(j);
-                if (c == '0' || c == '5') {
-                    sb.deleteCharAt(j);  
-                    j--;
-                }
-            }
-
-            if(sb.length() == 0){
+            if(String.valueOf(i).matches("[05]+")){
                 list.add(i);
             }
-            
         }
-        
         
         if(list.isEmpty()){
             return new int[]{-1};
