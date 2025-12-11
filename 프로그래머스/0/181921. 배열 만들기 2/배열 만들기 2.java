46776a63fc5222b5c6a38425e95fcd1ca380346f
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-11 22:13:25 +0900

    [level 0] Title: 배열 만들기 2, Time: 125.03 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -2,32 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int l, int r) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int count = 0;
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = l; i <= r; i++){
+            String s = String.valueOf(i);
+            StringBuilder sb = new StringBuilder(s);
 
-        for(int i = l; i <= r; i++) {
-            // 문자열로 변환
-            String str = String.valueOf(i);
-            boolean isAdd = true;
-            for (int j = 0; j < str.length(); j++) {
-                char ch = str.charAt(j);
-                if (ch != '0' && ch != '5') {
-                    isAdd = false;
-                    break;
+            for (int j = 0; j < sb.length(); j++) {
+                char c = sb.charAt(j);
+                if (c == '0' || c == '5') {
+                    sb.deleteCharAt(j);  
+                    j--;
                 }
             }
 
-            if (isAdd) {
-                answer.add(i);
+            if(sb.length() == 0){
+                list.add(i);
             }
+            
         }
-
-        if(answer.size() == 0) {
-            answer.add(-1);
+        
+        
+        if(list.isEmpty()){
+            return new int[]{-1};
         }
-
-        int[] newArr = answer.stream().mapToInt(Integer::intValue).toArray();
-        return newArr;
+        
+        return list.stream().mapToInt(i -> i).toArray();
     }
-    
 }
