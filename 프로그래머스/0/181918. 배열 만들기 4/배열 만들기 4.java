cb8eb304d3a227c800104afbdbe8dba02ceefda1
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-12 16:30:28 +0900

    [level 0] Title: 배열 만들기 4, Time: 26.47 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,29 +1,25 @@
 import java.util.*;
 
+
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
-        int i = 0;
-
-        while(i < arr.length){
-            if(stk.size() == 0) {
-                stk.add(arr[i]);
-                i++;
-            } else {
-                if(stk.get(stk.size()-1) < arr[i]){
-                    stk.add(arr[i]);
-                    i++;
-                } else {
-                    stk.remove(stk.size()-1);
-                }
-            }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i : arr){
+            
+            while(!list.isEmpty() && list.get(list.size()-1) >= i){
+                list.remove(list.size()-1);
+            } 
+            
+            list.add(i);
         }
-
-        int[] newArr = stk.stream().mapToInt(Integer::intValue).toArray();
-        return newArr;
+        
+        return list.stream().mapToInt(i -> i).toArray();
+        
+        
     }
 }
-// i < arr.length -> 다음 작업 반복
-// stk.length == 0 -> arr[i]를 stk 추가 -> i++
-// stk.length != 0 -> stk의 마지막원소 < arr[i] -> arr[i]를 stk의 뒤에 추가 -> i++
-// stk.length != 0 -> stk 마지막원소 >= arr[i] -> stk 마지막 원소를 제거
+
+
+  
