b6304186ee6b8cb357711f9f3983f0ff273a0636
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-16 16:27:15 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,6 +3,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        /*
+        // 1. 
+        // 1) 포켓몬 종류 : 3가지, 가질 수 있는 개수 : 2 => 2
+        // 2)           : 3    ,                : 3 => 3
+        // 3)           : 2    ,                : 3 => 2
+        
+        // 가질 수 있는 개수 > 포켓몬 종류 : 포켓몬 종류 답
+        //                 <           : 가지는 개수 답
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // 포켓몬 종류 개수 세기
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        
+        //System.out.println("포켓몬 개수 : " + map.size());
+        //System.out.println("가질 수 있는 개수 : " + (nums.length)/2);
+        
+        // 가질 수 있는 포켓몬 개수
+        int availableCnt =  nums.length/2;
+        int totalSpecies = map.size();
+        
+        return totalSpecies > availableCnt ? availableCnt : totalSpecies;
+        */
+
+       // 2. Set으로 포켓몬 중복 X 종류 세기
        Set<Integer> set = new HashSet<>();
         
         for(int i : nums){
