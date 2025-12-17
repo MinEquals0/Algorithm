0fbc8cad360d5e52c45865f48bac893970402fc3
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-17 15:57:27 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,13 +3,21 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false; 
+        
+        boolean answer = true;
+        
+        for(int i = 0; i < phone_book.length-1; i++){ 
+            
+            // 짧은 문자열.startsWith(긴 문자열)
+            boolean isPrefix = phone_book[i+1].startsWith(phone_book[i]);
+            
+            // 접두어로 있으면 F
+            if(isPrefix){
+                answer = false;
             }
+            
         }
-
-        return true;
+        
+        return answer;
     }
 }
