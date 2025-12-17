c00556657e585bf39b32cedc1fed7fd3b5599828
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-17 15:45:31 +0900

    [level 2] Title: 전화번호 목록, Time: 362.32 ms, Memory: 190 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,29 +2,18 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // for문 2개써서 포함여부 확인? 접두어니까 부분이 아니라 앞쪽에 위치하는지 봐야되네?
-        // contains 이런함수는 못쓰고
-        // 그럼 charAt() 이걸로..? 
-        // 해시는 어케 쓴담?
-        
-        Arrays.sort(phone_book);
-        
-        boolean answer = true;
-        
-        for(int i = 0; i < phone_book.length-1; i++){ 
-            
-            // 짧은 문자열.startsWith(긴 문자열)
-            boolean isPrefix = phone_book[i+1].startsWith(phone_book[i]);
-            
-            // 접두어로 있으면 F
-            if(isPrefix){
-                answer = false;
+        HashSet<String> set = new HashSet<>();
+        Arrays.stream(phone_book).forEach(s -> set.add(s));
+
+        for(String str : set){
+            for(int i = 1; i < str.length(); i++){
+                if(set.contains(str.substring(0, i))){
+                    return false;
+                }
             }
-            
         }
-        
-        return answer;
+
+        return true;
     }
 }
-
-
+  
