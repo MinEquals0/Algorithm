80a7fbfab5c260b917c311227d5af80394697627
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-17 16:14:22 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,6 +2,28 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        // 1. 배열 + 정렬
+       /* Arrays.sort(phone_book);
+        
+        boolean answer = true;
+        
+        for(int i = 0; i < phone_book.length-1; i++){ 
+            
+            // 짧은 문자열.startsWith(긴 문자열)
+            boolean isPrefix = phone_book[i+1].startsWith(phone_book[i]);
+            
+            // 접두어로 있으면 F
+            if(isPrefix){
+                answer = false;
+            }
+            
+        }
+        
+        return answer;
+        */
+
+
+        // 2. 문제에 맞게 해시 사용하기
         Set<String> set = new HashSet<>();
         
         // 1. 모든 전화번호를 Set에 저장
