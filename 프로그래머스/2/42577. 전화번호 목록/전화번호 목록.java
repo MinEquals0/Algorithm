a6303ce418a78d0156c72038013c28f5096be568
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-17 15:49:22 +0900

    [level 2] Title: 전화번호 목록, Time: 275.78 ms, Memory: 220 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,23 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        HashSet<String> set = new HashSet<>();
-        Arrays.stream(phone_book).forEach(s -> set.add(s));
+       HashMap<String,Integer> map = new HashMap<>();
+        for (String num : phone_book){
+            if (!map.containsKey(num)){
+                map.put(num, 0);
+            } else{
+                return false;
+            }
+        }
 
-        for(String str : set){
-            for(int i = 1; i < str.length(); i++){
-                if(set.contains(str.substring(0, i))){
+        for (String phone : phone_book){
+            for (int i = 0 ; i < phone.length() ; i++){
+                if(map.containsKey(phone.substring(0, i))){
                     return false;
                 }
             }
         }
+
 
         return true;
     }
