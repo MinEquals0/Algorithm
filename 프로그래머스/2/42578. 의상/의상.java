760b689b3099cb56029764f0aa81c8530ec190e1
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-18 16:49:41 +0900

    Update 의상.java

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        
+        // 곱의 법칙 + 부분집합 개념으로 접근
         
         Map<String, Integer> map = new HashMap<>();
         
@@ -12,7 +12,8 @@ class Solution {
             map.put(type, map.getOrDefault(type, 0)+1);
             
         }
-        
+
+        // 확인용 출력
         // for (String key : map.keySet()) {
         //      System.out.println("key = " + key + ", value = " + map.get(key));
         // }
@@ -24,7 +25,7 @@ class Solution {
             answer *= i + 1;
         }
             
-    
+        // 전부 공집합인 경우 제외 -1
         return answer - 1;
     }
 }
