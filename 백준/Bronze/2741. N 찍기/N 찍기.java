9972060fac2a620065b7161cbe88e23b5d7fedac
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-22 12:38:57 +0900

    [Bronze V] Title: N 찍기, Time: 1132 ms, Memory: 35104 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = Integer.parseInt(s);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
