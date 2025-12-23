d2d31aa56c1240b484049002780479204dc03aef
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-23 14:37:36 +0900

    [Bronze V] Title: 기찍 N, Time: 1092 ms, Memory: 31368 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = Integer.parseInt(s);
+
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
