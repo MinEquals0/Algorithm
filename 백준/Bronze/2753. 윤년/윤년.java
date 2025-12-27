5bf93f18c2ac7eeebe34ea2e84211616f487f5db
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-27 15:34:07 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        /*
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int year = Integer.parseInt(s);
@@ -16,8 +17,19 @@ public class Main {
 
         } else {
             System.out.println(0);
-        }
+        } 
+        */
 
+
+
+        // 최적화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        System.out.println(isLeap ? 1 : 0);
 
     }
+
 }
