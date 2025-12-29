e710e5996a12211f28edd23f3718ce988e006c7f
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-29 15:37:58 +0900

    Update 16진수.java

--- 백준/Bronze/1550. 16진수/16진수.java
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -5,10 +5,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+
+        /*
+        // 1. 함수 이용
         int num = Integer.parseInt(s, 16);
 
         System.out.println(num);
+        */
 
+        // 2. 직접 작성
+        int num = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int cValue = Character.digit(c,16);
+            num = num * 16 + cValue;
+        }
+        
+        System.out.println(num);
 
     }
 }
+
