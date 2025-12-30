d338b96fa31ae17feeb4068bc624454004b5399b
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2025-12-30 18:53:56 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        // for문으로 num_list 돌면서 n++ 증가하면서 요소 저장
-        // 요소를 저장할 배열을 생성 - arraylist
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
-        for(int i = 0; i < num_list.length; i += n){
+        for (int i = 0; i < num_list.length; i += n) {
             list.add(num_list[i]);
         }
-        return list.stream().mapToInt(t -> t).toArray();
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }
