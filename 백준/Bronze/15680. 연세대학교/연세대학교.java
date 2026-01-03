506aca80a4faaa3e8f4ff6bc063c94e040a6578b
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-01-03 12:19:08 +0900

    Update 연세대학교.java

--- 백준/Bronze/15680. 연세대학교/연세대학교.java
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -5,6 +5,7 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
+        //  String input = br.readLine().trim();  // 숫자 뒤에 공백이 올 경우 대비
         
         if(input.equals("0")){
             System.out.println("YONSEI");
@@ -15,4 +16,5 @@ class Main {
         
         
     }
+
 }
