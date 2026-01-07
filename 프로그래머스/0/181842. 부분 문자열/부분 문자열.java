c10ba7a710d87495fe747739a8f9e789528a45eb
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-01-07 17:55:14 +0900

    Update 부분 문자열.java

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
+        // substring 이용. str1길이만큼 문자열 잘라서 str2와 비교
+        // 함수에 사용될 인덱스를 다르게 설정 -> for 문
+      
   
         for(int i = 0; i <= str2.length()-str1.length(); i++){
 
