f4f4f2c1856b665fe3024d5a1d963e7726252997
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-01-08 14:01:11 +0900

    Update qr code.java

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,9 +1,9 @@
 import java.util.*;
-
+// import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int q, int r, String code) {
-        
+        // 1. StringBuilder
         StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < code.length(); i++){
@@ -11,8 +11,25 @@ class Solution {
                 sb.append(code.charAt(i));
             }
         }
-        
-
         return sb.toString();
+
+
+        /*
+        // 2. 리스트
+        ArrayList<Character> list = new ArrayList<>();
+        
+        for(int i = 0; i < code.length(); i++){
+            if(i % q == r){
+                list.add(code.charAt(i));
+            }
+        }
+        
+        String answer = list.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining());
+        
+        return answer;
+        */
+        
     }
 }
