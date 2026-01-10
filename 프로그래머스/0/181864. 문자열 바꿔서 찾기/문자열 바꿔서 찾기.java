bbc17c9d5ae89a180cb194e9fc364e0a14025805
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-01-10 12:42:58 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(String myString, String pat) {
-        // myString - replace 사용해서 A -> B, B -> A로 바꿔줌
-        // 문제점 - A를 B로 바꾼 다음에 B를 A로 바꾸려고 기존에 있던 B만 바뀌지 않음
-        // 두 개를 바꿔줄 때는 temp
-        // contains 사용해서 pat 있는지 확인 -> 있으면 1, 없으면 0
-
-        myString = myString.replace("A", "C");
-        myString = myString.replace("B", "A");
-        myString = myString.replace("C", "B");
-
-        return myString.contains(pat) ? 1 : 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.charAt(i) == 'A'){
+                sb.append("B");
+            } else {
+                sb.append("A");
+            }
+        }
+        
+        if(sb.toString().contains(pat)){
+            return 1;
+        }
+        return 0;
     }
 }
