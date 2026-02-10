26adda686b570abab019b015fc1eba517057f7ba
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-02-10 12:48:38 +0900

    Update 공통교육과정.java

--- 백준/Bronze/34813. 공통교육과정/공통교육과정.java
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.java
@@ -49,7 +49,45 @@ public class Main {
         System.out.println(map.get(subject));
 
 
+        // 4. Enum
+        char code = str.charAt(0);
+
+        SubJectArea area = SubJectArea.fromCode(code);
+
+        System.out.println(area.getDisplayName());
+
     }
 
 }
+
+// 4. Enum
+enum SubJectArea {
+    FOUNDATION('F', "Foundtaion"),
+    CLAVES('C', "Claves"),
+    VERITAS('V', "Veritas"),
+    EXPLORATION('E', "Exploration");
+
+    private final char code;
+    private final String displayName;
+
+    SubJectArea(char code, String displayName){
+        this.code = code;
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName(){
+        return displayName;
+    }
+
+    // 코드 -> enum 찾기
+    public static SubJectArea fromCode(char code){
+        for(SubJectArea area : values()){
+            if(area.code == code){
+                return area;
+            }
+        }
+        return null;
+    }
+}
+
 
