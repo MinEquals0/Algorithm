8768823381e807764679621891b157197e75e906
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-02-15 13:26:07 +0900

    [Bronze V] Title: Hello Judge, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < num; i++){
+            System.out.println("Hello World, Judge " + (i+1) + "!");
+        }
+       
+    }
+}
