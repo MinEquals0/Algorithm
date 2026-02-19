c7e49304e792c7518eb899be6baa256dbe8b0dd7
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-02-19 12:21:15 +0900

    Update 1672-richest-customer-wealth.java

--- LeetCode/Easy/1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ LeetCode/Easy/1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -13,6 +13,23 @@ class Solution {
         }
 
         return max;
-        
+
+
+        // 2. 더 짧은 시간
+        int highestWealth = 0;
+
+        for(int i = 0; i < accounts.length; i++){
+            int customerWealth = 0;
+
+            for(int j = 0; j < accounts[i].length; j++){
+                customerWealth += accounts[i][j];
+            }
+
+            if(customerWealth > highestWealth) {
+                highestWealth = customerWealth;
+            }
+        }
+
+        return highestWealth;
     }
 }
