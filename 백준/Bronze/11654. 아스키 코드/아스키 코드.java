e8317eddf68b1c0d14e694587184aca210443ab2
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-02-25 21:01:54 +0900

    Update 아스키 코드.java

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        char ch = br.readLine().charAt(0);  // 첫 글자 가져오기
+        char ch = br.readLine().charAt(0);  
         
-        System.out.println((int) ch);       // 아스키 코드 출력
+        System.out.println((int) ch);  
     }
+
 }
