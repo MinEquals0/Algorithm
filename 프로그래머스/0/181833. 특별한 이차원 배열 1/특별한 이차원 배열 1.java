44d2a62d6dd37e63d293647b764d979d7d6ea8ef
Min Eun-young <164611000+MinEquals0@users.noreply.github.com>
2026-04-14 16:36:56 +0900

    Update 특별한 이차원 배열 1.java

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        
+
+        // 1. 
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         if(i == j) {
+        //             answer[i][j] = 1;
+        //         } else {
+        //             answer[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+        // 2. 
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i == j) {
-                    answer[i][j] = 1;
-                } else {
-                    answer[i][j] = 0;
-                }
-            }
+          answer[i][i] = 1;
         }
         
         return answer;
